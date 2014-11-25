e6185be07e870c30cefedc0596e02cf35ae3a181
Афанасьев Дмитрий <afanasev@comefoto.ru>
2014-11-25 16:13:09 +0600

    версия 2.1.1

--- FormClientUnit.cpp
+++ FormClientUnit.cpp
@@ -26,7 +26,7 @@ __fastcall TfConnect::TfConnect(TComponent* Owner) : TForm(Owner) {
 	curUser = new TBasicUser("Не подключен");
 	listOfPriceType = new TList();
 
-	version = "2.1";
+	version = "2.1.1";
 	Caption = "Подключение к 1с. Версия " + version;
 }
 
